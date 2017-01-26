20e51ec1f6074a82ee28512c4bc2abf7818abdaa
Tushar <saha.tushar091@gmail.com>
2017-01-26 12:52:26 +0530

    server

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-var http = require('http');
-fs = require('fs');
-fs.readFile('./hello.html',function(err,html){
-            if(err){
-            throw err;
-            }
-            http.createServer(function(req, res) {
-                              res.writeHead(200,{"Content-Type":"text/html"});
-                              res.write(html);
-                              res.end();
-            }).listen(8080);
-            });
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
+
+var server = http.createServer(function(request, response) {
+                               var filePath = false;
+                               
+                               if (request.url == '/') {
+                               filePath = "hello.html";
+                               } else {
+                               filePath = "public" + request.url;
+                               }
+                               
+                               var absPath = "./" + filePath;
+                               serverWorking(response, absPath);
+                               });
+var port_number = server.listen(process.env.PORT || 8080);
+
+server.listen(port_number)
+function send404(response) {
+    response.writeHead(404, {"Content-type" : "text/plain"});
+    response.write("Error 404: resource not found");
+    response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+    response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+    fs.exists(absPath, function(exists) {
+              if (exists) {
+              fs.readFile(absPath, function(err, data) {
+                          if (err) {
+                          send404(response)
+                          } else {
+                          sendPage(response, absPath, data);
+                          }
+                          });
+              } else {
+              send404(response);
+              }
+              });
+}
