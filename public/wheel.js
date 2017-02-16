d990832f95b4b873f2474d8eca4913aa50664733
Tushar <saha.tushar091@gmail.com>
2017-02-17 00:11:41 +0530

    webpage final design

--- public/wheel.js
+++ public/wheel.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('mycanvas'),
-context = canvas.getContext('2d');
-w = 500;h = 500;
+    context = canvas.getContext('2d');
+w = 500;
+h = 500;
 canvas.width = w;
 canvas.height = h;
 var radius = 45;
@@ -13,48 +14,48 @@ var initialAngle = 30;
 var j = 1;
 
 wheel = {
-x:250,
-y:250,
-    
-radiusOut:45,
-radiusIn:35,
-    
-    draw : function(){
+    x: 250,
+    y: 250,
+
+    radiusOut: 45,
+    radiusIn: 35,
+
+    draw: function() {
         context.beginPath();
-        context.arc(250, 250, radius, 0, Math.PI*2, false);
+        context.arc(250, 250, radius, 0, Math.PI * 2, false);
         context.fillStyle = 'white';
         context.fill();
         context.closePath();
-        
+
         context.beginPath();
-        context.arc(250, 250, 35, 0, Math.PI*2, false);
+        context.arc(250, 250, 35, 0, Math.PI * 2, false);
         context.fillStyle = 'black';
         context.fill();
         context.closePath();
-        
+
         context.beginPath();
-        context.arc(250, 250, radius + 5, 0, Math.PI*2, false);
+        context.arc(250, 250, radius + 5, 0, Math.PI * 2, false);
         context.strokeStyle = 'white'
         context.stroke();
         context.closePath();
-        
+
         context.beginPath();
-        context.arc(250, 250, radius + 15, 0, Math.PI*2, false);
+        context.arc(250, 250, radius + 15, 0, Math.PI * 2, false);
         context.strokeStyle = 'white'
         context.stroke();
         context.closePath();
-        
-        for(i = 0;i<12;i++){
+
+        for (i = 0; i < 12; i++) {
             ang = initialAngle * i;
             context.beginPath();
-            context.moveTo(this.x,this.y);
-            context.lineTo(this.x + Math.cos(Math.PI*(ang + angle)/180)*this.radiusIn,
-                           this.y + Math.sin(Math.PI*(ang + angle)/180)*this.radiusIn);
+            context.moveTo(this.x, this.y);
+            context.lineTo(this.x + Math.cos(Math.PI * (ang + angle) / 180) * this.radiusIn,
+                this.y + Math.sin(Math.PI * (ang + angle) / 180) * this.radiusIn);
             context.strokeStyle = 'white'
             context.lineWidth = 5;
             context.stroke();
         }
-        
+
     }
 }
 
@@ -62,7 +63,7 @@ function clearCanvas() {
     context.clearRect(0, 0, w, h);
 }
 
-function refresh(){
+function refresh() {
     clearCanvas();
     angle = j;
     j++;
@@ -70,9 +71,30 @@ function refresh(){
     wheel.draw();
 }
 
-setTimeout(function(){
-           var element = document.getElementById("mainBody");
-           element.classList.add("loaded");
-           }, 3000);
+setTimeout(function() {
+    var element = document.getElementById("mainBody");
+    element.classList.add("loaded");
+}, 3000);
 
-setInterval(refresh,1000/60);
+setInterval(refresh, 1000 / 60);
+
+var sections = $('section'),
+    nav = $('nav'),
+    nav_height = nav.outerHeight();
+
+$(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').removeClass('active');
+            sections.removeClass('active');
+
+            $(this).addClass('active');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+    });
+});
