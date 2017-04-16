fe98c6321b443ff9fb83da7ad76d9b28f7bbd417
Tushar <saha.tushar091@gmail.com>
2017-04-17 00:43:46 +0530

    images

---
+++ public/like.js
@@ -0,0 +1,13 @@
+var x, y;
+
+var canvas = document.getElementById("hobbies_canvas");
+var context = canvas.getContext("2d");
+
+
+
+function draw() {
+    context.rect(0, 0, 10, 30);
+    context.stroke();
+};
+
+draw();
